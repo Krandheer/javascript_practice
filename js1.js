c31e5336b5fc3e69c74c575dc08147fe6598bffd
Randheer kumar <randheer.dakshana15@gmail.com>
2023-06-10 20:01:44 +0530

    first js probably

---
+++ js1.js
@@ -0,0 +1,42 @@
+const sumAll = (startIndex, endIndex) => {
+    let total = 0
+    for (let i = startIndex; i <= endIndex; i++) {
+        total = total+i
+    }
+    return total
+}
+// console.log(sumAll(1, 100))
+
+
+// using map
+let users = [{name: 'harkirat', age: 22}, {name: 'randheer', age: 28}]
+function printAllUsers(user){
+    user.map(item => {
+    console.log(`${item.name} is of age: ${item.age}`)
+})}
+
+// printAllUsers(users)
+
+// fib in javascript
+function fib(num){
+    if (num <= 1){
+        return num
+    }
+    return fib(num-1)+fib(num-2)
+}
+// for (let i = 0; i<6; i++){
+//     console.log(fib(i))
+// }
+
+
+// pattern printing in function
+function createePattern(numberOfRows) {
+    for (let i = 0; i < numberOfRows; i++) {
+       let stringToPrint = ""
+       for (let j = 0; j < i+1; j ++){
+            stringToPrint = stringToPrint+"*"
+       }
+        console.log(stringToPrint)
+    }
+}
+createePattern(10)
